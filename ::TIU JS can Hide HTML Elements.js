ddcc9cdcdaa0b1229090e09decbf6f4dfdfe622c
Piyush <thakarepiyush06@gmail.com>
2017-01-25 19:52:27 +0530

    Day 2

---
+++ ::TIU JS can Hide HTML Elements.js
@@ -0,0 +1,15 @@
+//TIU JS can Hide HTML Elements
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>What Can JavaScript Do?</h1>
+
+<p id="demo">JavaScript can hide HTML elements.</p>
+
+<button type="button" onclick="document.getElementById('demo').style.display='none'">Click Me!</button>
+
+
+</body>
+</html>
---
+++ JS can Hide HTML elements.js
@@ -0,0 +1,4 @@
+//JS can hide HTML Elements
+//Hiding HTML elements can be done by changing the display style:
+
+document.getElementById("demo") .style.display = "none";
