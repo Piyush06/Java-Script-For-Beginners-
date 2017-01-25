466e72516c2e2d5a752e32ea99ff7d26a7954d1b
Piyush <thakarepiyush06@gmail.com>
2017-01-25 19:47:11 +0530

    Day 2

---
+++ JS can change HTML Styles
@@ -0,0 +1,6 @@
+//Javascript can change HTML Styles (CSS)
+//changing the style of an HTML element, is a variant of changing an HTML attribute:
+
+document.getElementBYId("demo") .styles.fontSize = "25px";
+
+//You can change the coefficient values as per your requirements or to test your Code.
---
+++ TIU(JS can Change HTML Styles).js
@@ -0,0 +1,15 @@
+//Try it yourself (JS can change HTML Styles)
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>What Can JavaScript Do?</h1>
+
+<p id="demo">JavaScript can change the style of an HTML element.</p>
+
+<button type="button" onclick="document.getElementById('demo').style.fontSize='35px'">Click Me!</button>
+
+
+</body>
+</html>
