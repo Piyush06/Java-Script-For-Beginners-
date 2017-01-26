559e1bec5056fe86de5c1553117769ac4bd4047a
Piyush <thakarepiyush06@gmail.com>
2017-01-26 15:13:56 +0530

    DAY 3

---
+++ DIY JS can Show Hidden HTML Elments.js
@@ -0,0 +1,16 @@
+//Do It Yourself JS can Shoe HTML Elements
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>What Can JavaScript Do?</h1>
+
+<p>JavaScript can show hidden HTML elements.</p>
+
+<p id="demo" style="display:none">Hello JavaScript!</p>
+
+<button type="button" onclick="document.getElementById('demo').style.display='block'">Click Me!</button>
+
+</body>
+</html>
---
+++ JS Can Show HTML Elements.js
@@ -0,0 +1,4 @@
+//JS can Show HTML elements
+//Showing hidden HTML elements can also be done by changing the display style:
+
+document.getElementById("demo").style.display = "block";
