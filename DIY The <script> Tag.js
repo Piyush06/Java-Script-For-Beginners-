f910ffeafdfe163f1108a8b7adedc05af502b0d8
Piyush <thakarepiyush06@gmail.com>
2017-01-28 11:49:23 +0530

    Day 4

---
+++ DIY The <script> Tag.js
@@ -0,0 +1,19 @@
+//DIY the Script Tag
+//Do it yourself in a editor of ur choice..
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>JavaScript in Body</h1>
+
+<p id="demo"></p>
+
+<script>
+document.getElementById("demo").innerHTML = "My First JavaScript";
+</script>
+
+</body>
+</html>
+
+//Old JavaScript examples may use a type attribute: <script type="text/javascript">.
+//The type attribute is not required. JavaScript is the default scripting language in HTML.
---
+++ The <script tag>.js
@@ -0,0 +1,6 @@
+//The <script> Tag
+//As in HTML we have container elements such as <p></p> in the same way code must be inserted between <script> and </script> tags
+
+<script>
+document.getElementById("demo").innerHTML = "My First JavaScript";
+</script>
